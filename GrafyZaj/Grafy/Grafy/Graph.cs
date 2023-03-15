a294a6aae76a0dab853a0648354f0c4f488a135c
Klaudia O <62263707+Loxx-KO@users.noreply.github.com>
2023-03-15 17:41:29 +0100

    Add files via upload

---
+++ GrafyZaj/Grafy/Grafy/Graph.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace Grafy
+{
+    class Graph
+    {
+        private List<Node> nodes;
+        private bool twoSidedNeighbour;
+
+        public Graph()
+        {
+            nodes = new List<Node>();
+        }
+
+        public Graph(bool _twoSideNeighbours)
+        {
+            nodes = new List<Node>();
+            twoSidedNeighbour = _twoSideNeighbours;
+        }
+
+        public void AddNode(int _value)
+        {
+            if(nodes.Count == 0)
+            {
+                nodes.Add(new Node(_value));
+            }
+            else if (FindNode(_value) == null)
+            {
+                nodes.Add(new Node(_value));
+            }
+        }
+
+        public void RemoveNode(int _value)
+        {
+            foreach (Node node in nodes)
+            {
+                if (_value == node.GetValue()) { nodes.Remove(node); break; }
+            }
+        }
+
+        public Node FindNode(int _value)
+        {
+            foreach (Node node in nodes)
+            {
+                if (_value == node.GetValue()) return node;
+            }
+            return null;
+        }
+
+        public void AddNeighbour(int _nodeValue, int _neighbour, int _edgeValue = 0)
+        {
+            Node neighborNode = FindNode(_neighbour);
+            if (neighborNode != null && FindNode(_nodeValue) != null)
+            {
+                if (twoSidedNeighbour)
+                {
+                    nodes[_nodeValue - 1].AddNeighbor(neighborNode, _edgeValue);
+                    nodes[neighborNode.GetValue() - 1].AddNeighbor(nodes[_nodeValue - 1], _edgeValue);
+                }
+                else
+                {
+                    nodes[_nodeValue - 1].AddNeighbor(neighborNode, _edgeValue);
+                }
+            }
+        }
+
+        public void RemoveNeighbour(int _nodeValue, int _neighbour)
+        {
+            Node neighborNode = FindNode(_neighbour);
+            if (neighborNode != null && FindNode(_nodeValue) != null)
+            {
+                if (twoSidedNeighbour)
+                {
+                    nodes[_nodeValue - 1].RemoveNeighbor(neighborNode);
+                    nodes[neighborNode.GetValue() - 1].RemoveNeighbor(nodes[_nodeValue - 1]);
+                }
+                else
+                {
+                    nodes[_nodeValue - 1].RemoveNeighbor(neighborNode);
+                }
+            }
+        }
+
+        public void ShowGraph()
+        {
+            Console.WriteLine("#DIGRAPH");
+            Console.WriteLine(twoSidedNeighbour);
+            Console.WriteLine("#EDGES");
+
+            foreach (Node node in nodes)
+            {
+                node.ShowContents();
+            }
+        }
+
+        public void ReadFile(string filepath)
+        {
+            int cnt = 0;
+            foreach (string line in System.IO.File.ReadLines(filepath))
+            {
+                if (cnt != 0 && cnt != 2)
+                {
+                    if (cnt == 1)
+                    {
+                        if (line == "false") twoSidedNeighbour = false;
+                        else if (line == "true") twoSidedNeighbour = true;
+                    }
+                    else 
+                    {
+                        string[] values = line.Split(" ");
+                        int begin = int.Parse(values[0]);
+                        int end = int.Parse(values[1]);
+                        int edge = int.Parse(values[2]);
+
+                        AddNode(begin);
+                        AddNode(end);
+                        AddNeighbour(begin, end, edge);
+                    }
+                }
+                cnt++;
+            }
+        }
+
+        public void SaveFile(string filepath)
+        {
+            string text = "#DIGRAPH\n";
+            text += twoSidedNeighbour.ToString() + "\n#EDGES\n";
+            
+            foreach(Node node in nodes)
+            {
+                text += node.SaveFile();
+            }
+            
+            File.WriteAllText(filepath, text);
+        }
+    }
+}
+
---
+++ GrafyZaj/Grafy/Grafy/Node.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Grafy
+{
+    class Node
+    {
+        public int Value;
+        public List<int> Neighbors;
+        public Dictionary<int, int> EdgeValues;
+
+        public Node(int _value)
+        {
+            Value = _value;
+            Neighbors = new List<int>();
+            EdgeValues = new Dictionary<int, int>();
+        }
+
+        public void SetValue(int _newVal)
+        {
+            Value = _newVal;
+        }
+
+        public int GetValue()
+        {
+            return Value;
+        }
+
+        public void AddNeighbor(Node _neighbor, int _edgeValue)
+        {
+            bool alreadyANeighbor = false;
+            foreach (int node in Neighbors)
+            {
+                if (_neighbor.GetValue() == node)
+                {
+                    Console.WriteLine("Already a neighbor");
+                    alreadyANeighbor = true;
+                    break;
+                }
+            }
+
+            if (!alreadyANeighbor)
+            {
+                Neighbors.Add(_neighbor.GetValue());
+                EdgeValues.Add(_neighbor.GetValue(), _edgeValue);
+            }
+        }
+
+        public void RemoveNeighbor(Node _neighbor)
+        {
+            foreach (int node in Neighbors)
+            {
+                if (_neighbor.GetValue() == node)
+                {
+                    Neighbors.Remove(_neighbor.GetValue());
+                    EdgeValues.Remove(_neighbor.GetValue());
+                    break;
+                }
+            }
+        }
+
+        public List<int> GetNeighbors()
+        {
+            return Neighbors;
+        }
+
+        /*public int GetNeighborValue()
+        {
+            int NeighbourValue = 0;
+            foreach (Node node in neighbours)
+            {
+                NeighbourValue = node.GetValue();
+                break;
+            }
+
+            if (NeighbourValue == 0) Console.WriteLine("Neighbor does not exist\n");
+
+            return NeighbourValue;
+        }*/
+
+        public string GetNeighborsValues()
+        {
+            string values = " ";
+
+            int iter = 0;
+            foreach (int node in Neighbors)
+            {
+                if (iter != Neighbors.Count - 1) values += node + ", ";
+                else values += node;
+
+                iter++;
+            }
+
+            return values;
+        }
+
+        public void ShowContents()
+        {
+            //Console.WriteLine("wierzchołek_początkowy wierzchołek_końcowy waga_krawędzi");
+            foreach (int neighbor in Neighbors)
+            {
+                Console.WriteLine(Value + " " + neighbor + " " + EdgeValues[neighbor]);
+            }
+        }
+
+        public string SaveFile()
+        {
+            string edges = "";
+            foreach (int neighbor in Neighbors)
+            {
+                edges += Value + " " + neighbor + " " + EdgeValues[neighbor] + "\n";
+            }
+
+            return edges;
+        }
+    }
+}
+
